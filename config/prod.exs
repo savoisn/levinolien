0cee275ec2683932ba9a0e4468721ea803cd65dc
savoisn <nicolas.savois@gmail.com>
2017-02-13 22:03:50 +0100

    typo

--- config/prod.exs
+++ config/prod.exs
@@ -26,7 +26,7 @@ config :logger, level: :info
 config :appli, Appli.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
 
