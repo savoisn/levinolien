4cea99828b1f702a3362fd002a36ec00e42f127b
savoisn <nicolas.savois@gmail.com>
2017-02-15 22:49:36 +0100

    adding email and count

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-npm install && node node_modules/brunch/bin/brunch build
-MIX_ENV=prod mix deps
-MIX_ENV=prod mix compile
-MIX_ENV=prod mix ecto.migrate 
-MIX_ENV=prod mix phoenix.server
+npm install && node node_modules/brunch/bin/brunch build --production
+MIX_ENV=prod_cc mix deps
+MIX_ENV=prod_cc mix compile
+MIX_ENV=prod_cc mix ecto.migrate 
+MIX_ENV=prod_cc mix phoenix.server
