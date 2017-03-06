50b1076386f4b7ad7afe353aa740cbf508055d18
savoisn <nicolas.savois@gmail.com>
2017-03-06 19:29:56 +0100

    refactored papers

--- web/controllers/papers_controller.ex
+++ web/controllers/papers_controller.ex
@@ -2,6 +2,17 @@ defmodule Appli.PapersController do
   use Appli.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    papers = [
+      %{:url => "images/papers/signatures_papier_1.jpg", :nb => 18},
+      %{:url => "images/papers/signatures_papier_2.jpg", :nb => 15},
+      %{:url => "images/papers/signatures_papier_3.jpg", :nb => 20},
+      %{:url => "images/papers/signatures_papier_4.jpg", :nb => 16},
+      %{:url => "images/papers/signatures_papier_5.jpg", :nb => 13},
+      %{:url => "images/papers/signatures_papier_6.jpg", :nb => 22},
+      %{:url => "images/papers/signatures_papier_7.jpg", :nb => 18},
+      %{:url => "images/papers/signatures_papier_8.jpg", :nb => 13},
+      %{:url => "images/papers/signatures_papier_9.jpg", :nb => 22},
+    ]
+    render conn, "index.html", papers: papers
   end
 end
