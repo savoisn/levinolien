4c28d072f3a18e6a218a7b63d266e0a1a5e2b501
savoisn <nicolas.savois@gmail.com>
2017-03-11 00:22:56 +0100

    running ecto migrate in entrypoint

--- Dockerfile
+++ Dockerfile
@@ -21,4 +21,4 @@ ADD . .
 RUN brunch build --production && \
     mix do compile, phoenix.digest
 
-CMD ["mix", "phoenix.server"]
+CMD ["entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-npm install && node node_modules/brunch/bin/brunch build --production
-MIX_ENV=prod_cc mix deps
-MIX_ENV=prod_cc mix compile
 MIX_ENV=prod_cc mix ecto.migrate 
 MIX_ENV=prod_cc mix phoenix.server
